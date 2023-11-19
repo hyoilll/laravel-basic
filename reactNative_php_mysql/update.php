9ab25ce33059ca389a6018e2bb6dc721cc8fcf79
hyoil Lee <dlgydlf123@gmail.com>
2023-11-19 20:55:10 +0900

    [ReactNative-PHP] update of DB

---
+++ reactNative_php_mysql/update.php
@@ -0,0 +1,33 @@
+<?php
+    require_once("./db_connection.php");
+
+    // jsonデータを受け取る。
+    $datas = json_decode(file_get_contents("php://input"));
+    
+    try{
+        $pdo->beginTransaction();
+
+        // select
+        $sql = "SELECT * from user WHERE name = :name";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(":name", $datas->name, PDO::PARAM_STR);
+        $stmt->execute();
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        // update
+        $sql = "UPDATE user SET email = :email WHERE name = :name";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(":email", $datas->email, PDO::PARAM_STR);
+        $stmt->bindValue(":name", $datas->name, PDO::PARAM_STR);
+        $stmt->execute();
+
+        // JSON 形式で結果を出力
+        header('Content-Type: application/json');
+        echo json_encode($results);
+
+        $pdo->commit();
+    } catch(PDOException $e){
+        echo "Connection failed: " . $e->getMessage();
+        $pdo->rollback();
+    }
+    
