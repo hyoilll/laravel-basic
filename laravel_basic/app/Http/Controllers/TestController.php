d299d226635a25c089982888c6f69b91264f7fe7
hyoil Lee <dlgydlf123@gmail.com>
2023-11-26 15:39:00 +0900

    [laravel_basic] get from db (Eloquent)

--- laravel_basic/app/Http/Controllers/TestController.php
+++ laravel_basic/app/Http/Controllers/TestController.php
@@ -8,11 +8,19 @@ use App\Models\Test;
 class TestController extends Controller
 {
     public function index(){
+        // Eloquent(エロくアント)方式
         // Testモーデルを介してtestsテーブルからデータを全て取ってくる。
         $values = Test::all();
 
+        // collection型の取説
+        // https://readouble.com/laravel/9.x/ja/collections.html#method-count
+
+        $count = Test::count();
+        $first_value = Test::findOrFail(1);
+        $where = Test::where("text", "hello")->get();
+
         // ここでコードの実行を一時止めて、$valuesを出力する。die + var_dump
-        // dd($values);
+        dd($values, $count, $first_value, $where);
 
         // compactを利用して、view側に$valuesを渡す。
         // compact：指定された変数名に対応する変数とその値から配列を作成する関数。
