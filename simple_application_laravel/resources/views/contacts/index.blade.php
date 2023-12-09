83cccbf6f67148df9b5ee8cd7748e585e71e75fe
hyoil Lee <dlgydlf123@gmail.com>
2023-12-09 16:26:16 +0900

    [simpleApp] update view of contacts.index

--- simple_application_laravel/resources/views/contacts/index.blade.php
+++ simple_application_laravel/resources/views/contacts/index.blade.php
@@ -9,8 +9,35 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("hello world") }}
                     <a href="{{ route('contacts.create') }}" class="text-blue-500">新規登録</a>
+                    <div class="flex flex-col text-center w-full mb-10">
+                        <h1 class="sm:text-4xl text-3xl font-medium title-font mb-2 text-gray-800 dark:text-gray-200">問い合わせ一覧</h1>
+                    </div>
+                    <div class="lg:w-2/3 w-full mx-auto overflow-auto">
+                    <table class="table-auto w-full text-left whitespace-no-wrap">
+                        <thead>
+                        <tr>
+                            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">ID</th>
+                            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">NAME</th>
+                            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">TITLE</th>
+                            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">CREATED_AT</th>
+                            <th class="w-10 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($contacts as $contact)
+                        <tr class="border-b-2 border-gray-200">
+                            <td class="px-4 py-3">{{ $contact->id }}</td>
+                            <td class="px-4 py-3">{{ $contact->name }}</td>
+                            <td class="px-4 py-3">{{ $contact->title }}</td>
+                            <td class="px-4 py-3 text-lg">{{ $contact->created_at }}</td>
+                            <td class="w-10 text-center">
+                            </td>
+                        </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                    </div>
                 </div>
             </div>
         </div>
