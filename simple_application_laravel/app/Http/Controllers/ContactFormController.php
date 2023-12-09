38bf240d147417a2541acb0963fd5a5bba10ab22
hyoil Lee <dlgydlf123@gmail.com>
2023-12-09 17:30:06 +0900

    [simpleApp] Convert gender and age to string

--- simple_application_laravel/app/Http/Controllers/ContactFormController.php
+++ simple_application_laravel/app/Http/Controllers/ContactFormController.php
@@ -51,7 +51,34 @@ class ContactFormController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $contact = ContactForm::find($id);
+
+        $gender = $contact->gender === 0 ? "Male" : "Female";
+
+        switch ($contact->age){
+            case 1:
+                $age = "~19";
+                break;
+            case 2:
+                $age = "20~29";
+                break;
+            case 3:
+                $age = "30~39";
+                break;
+            case 4:
+                $age = "40~49";
+                break;
+            case 5:
+                $age = "50~59";
+                break;
+            case 6:
+                $age = "60~";
+                break;
+            default:
+                break;
+        }
+
+        return view("contacts.show", compact("contact", "gender", "age"));
     }
 
     /**
